d255667ffae3c98df4c38a2bf718e8dbe962a628
Jake Demian <jakedemian@gmail.com>
2017-12-30 09:39:52 -0500

    finished super basic player movement and control

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -15,35 +15,31 @@ public class PlayerController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		float vert = Input.GetAxisRaw("Vertical");
-		float horiz = Input.GetAxisRaw("Horizontal");
+		Vector3 camForward = cam.transform.forward.normalized;
+		Vector3 camRight = cam.transform.right.normalized;
 
-		if(vert == 1){
-			// get camera facing direction
-			Vector3 dirRaw = cam.transform.forward.normalized;
-			Vector3 forward = new Vector3(dirRaw.x, 0, dirRaw.z); // horrible i'm gunna puke pls fix
-			transform.Translate(forward * Time.deltaTime * TEMP__PLAYER_RAW_FORWARD_MOVE_SPEED);
-		} else if(vert == -1){
-			Vector3 dirRaw = cam.transform.forward.normalized;
-			Vector3 backward = new Vector3(dirRaw.x, 0, dirRaw.z) * -1; // horrible i'm gunna puke pls fix
-			transform.Translate(backward * Time.deltaTime * TEMP__PLAYER_RAW_BACKWARD_MOVE_SPEED);
+		Vector3 verticalVector = Vector3.zero;
+		Vector3 horizontalVector = Vector3.zero;
+		Vector3 movementVector = Vector3.zero;
+
+		if(Input.GetAxisRaw("Vertical") == 1){
+			verticalVector = new Vector3(camForward.x, 0, camForward.z);
+		} else if(Input.GetAxisRaw("Vertical") == -1){
+			verticalVector = new Vector3(camForward.x, 0, camForward.z) * -1;
 		}
 
-		if(horiz == 1){
-			Vector3 dirRaw = cam.transform.right.normalized;
-			Vector3 right = new Vector3(dirRaw.x, 0, dirRaw.z); // horrible i'm gunna puke pls fix
-			transform.Translate(right * Time.deltaTime * TEMP__PLAYER_RAW_FORWARD_MOVE_SPEED);
-		} else if(horiz == -1){
-			Vector3 dirRaw = cam.transform.right.normalized;
-			Vector3 left = new Vector3(dirRaw.x, 0, dirRaw.z) * -1; // horrible i'm gunna puke pls fix
-			transform.Translate(left * Time.deltaTime * TEMP__PLAYER_RAW_FORWARD_MOVE_SPEED);
+		if(Input.GetAxisRaw("Horizontal") == 1){
+			horizontalVector = new Vector3(camRight.x, 0, camRight.z);
+		} else if(Input.GetAxisRaw("Horizontal") == -1){
+			horizontalVector = new Vector3(camRight.x, 0, camRight.z) * -1;
 		}
 
-		Debug.Log("don't forget to do you TODO you stupid idiot but i'm tired so i'm calling it quits for tonight <3");
+		if(Input.GetAxisRaw("Horizontal") != 0 && Input.GetAxisRaw("Vertical") != 0){
+			horizontalVector /= Mathf.Sqrt(2f);
+			verticalVector /= Mathf.Sqrt(2f);
+		}
 
-		// TODO i need to refactor this a bit.  firstly, i'm repeating the same thing 4 times so it should probly be a methor or something.
-		// also i have to fix the diagonal bug, so i need to calculate the values but NOT instantly Translate.  get both the horizontal and the vertical
-		// axis calculation complete, save them, then at the end of Update() do the Translate.  if horizontal and vertical movements are present, I need
-		// to clamp the values by dividing (maybe multiplying, idr do the math you moron) by Mathf.Sqrt(2)
+		movementVector = horizontalVector + verticalVector;
+		transform.Translate(movementVector * Time.deltaTime * TEMP__PLAYER_RAW_FORWARD_MOVE_SPEED);
 	}
 }
---
+++ Assets/PlayerJumpController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerJumpController : MonoBehaviour {
+	public float jumpSpeed = 5f;
+	Rigidbody rb;
+
+	void Start(){
+		rb = GetComponent<Rigidbody>();
+	}
+
+	void Update () {
+		if(Input.GetButtonDown("Jump")){
+			rb.velocity += Vector3.up * jumpSpeed;
+		}
+	}
+}
