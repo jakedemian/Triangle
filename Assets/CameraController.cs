b237576e9aa44722fd86c9b288fc0a7513e78c21
Jake Demian <jakedemian@gmail.com>
2017-12-31 10:12:34 -0500

    added a test scene for building the island

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -22,10 +22,30 @@ public class CameraController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		transform.LookAt(player.transform);
-		if(Input.GetMouseButton(1) && Input.GetAxis("Mouse X") != 0){
-			float orbitSpeed = orbitSpeedFactor * Input.GetAxis("Mouse X") * Time.deltaTime;
-			transform.RotateAround(player.transform.position, Vector3.up, orbitSpeed);
+		if(Input.GetMouseButton(1)){
+			if(Input.GetAxis("Mouse X") != 0){
+				float orbitSpeed = orbitSpeedFactor * Input.GetAxis("Mouse X") * Time.deltaTime;
+				transform.RotateAround(player.transform.position, Vector3.up, orbitSpeed);
+			}
+			if(Input.GetAxis("Mouse Y") != 0 && cameraIsNotAtLimits()){
+				float orbitSpeed = orbitSpeedFactor * Input.GetAxis("Mouse Y") * Time.deltaTime;
+				transform.RotateAround(player.transform.position, Vector3.right, orbitSpeed * -1);
+
+				if(transform.position.y < player.transform.position.y){
+					transform.position = new Vector3(transform.position.x, player.transform.position.y, transform.position.z);
+				}
+			}
 		}
 		lastMousePosition = Input.mousePosition;
+
+		if(Input.GetAxis("Mouse ScrollWheel") != 0){
+			GetComponent<Camera>().fieldOfView -= Input.GetAxis("Mouse ScrollWheel") * Time.deltaTime * 1000f;
+		}
+	}
+
+	private bool cameraIsNotAtLimits(){
+		bool lowerLimitCheck = Input.GetAxis("Mouse Y") < 0 || transform.position.y > player.transform.position.y;
+		bool upperLimitCheck = Input.GetAxis("Mouse Y") > 0 || Mathf.Abs(Vector3.Angle(transform.forward, -Vector3.up)) > 10f;
+		return lowerLimitCheck && upperLimitCheck;
 	}
 }
